942ea94869d1219ab02f17edd208a16a6fd98156
Mohammad Ilham Nurdhi Hafsyah <ilhamhafsyah15@gmail.com>
2021-01-28 07:00:49 +0700

    install standard

---
+++ src/controller/flight.js
@@ -0,0 +1,24 @@
+const helper = require("../helper/response");
+
+module.exports = {
+  postFlight: async (req, res) => {
+    try {
+      const {
+        mascapai,
+        facilities,
+        departureTime,
+        arrivedTime,
+        price,
+        capacity,
+        class,
+        fromCity,
+        fromCountry,
+        toCity,
+        toCountry,
+        tripType,
+        terminal,
+        transitType,
+      };
+    } catch (error) {}
+  },
+};
