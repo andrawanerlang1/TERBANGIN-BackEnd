ac85fc3849209bd1e5c6b42a7450d2fcd7d43290
Mohammad Ilham Nurdhi Hafsyah <ilhamhafsyah15@gmail.com>
2021-02-01 17:18:04 +0700

    repair auth for admin and implement redis

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,8 @@ const helper = require('../helper/response')
 
 module.exports = {
   authUser: (request, response, next) => {
-    let token = request.headers.authUser
+    let token = request.headers.authorization
+    console.log(request.headers)
     console.log(token)
     if (token) {
       token = token.split(' ')[1]
@@ -25,29 +26,15 @@ module.exports = {
     }
   },
   authrole1: (request, response, next) => {
-    let token = request.headers.authrole1
-    token = token.split(' ')[1]
-    jwt.verify(token, 'TERBANGIN', (error, result) => {
-      if (
-        (error && error.name === 'JsonWebTokenError') ||
-        (error && error.name === 'TokenExpiredError')
-      ) {
-        console.log(error)
-        return helper.response(response, 400, error.message)
-      } else {
-        console.log(result)
-
-        if (result.role === 1) {
-          request.token = result
-          next()
-        } else {
-          return helper.response(
-            response,
-            202,
-            'Not Allowed ! Page accessible by admin only'
-          )
-        }
-      }
-    })
+    console.log(request.token.role)
+    if (request.token.role === 0) {
+      return helper.response(
+        response,
+        400,
+        'Not Allowed ! Page accessible by admin only'
+      )
+    } else {
+      next()
+    }
   }
 }
